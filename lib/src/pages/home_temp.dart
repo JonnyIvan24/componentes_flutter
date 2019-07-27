fcf2d02fc176612a702cd2ce2dae89055e29e024
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-07-27 13:43:57 -0500

    llenar lista  por medio de un metodo

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
 
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,20 +11,29 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-        ],
+        children: _crearItems()
       ),
     );
   }
+
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones){
+
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      // al poner .. lo que hace es tomar el objeto anterior para seguir dando instrucciones algo parecido a...
+      // lista.add(tempWidget)
+      // lista.add(Divider());
+      lista..add(tempWidget)
+            ..add(Divider());
+    }
+    
+    return lista;
+  }
+
 }
