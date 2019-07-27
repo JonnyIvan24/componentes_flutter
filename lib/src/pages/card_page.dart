85375a126957e3e04f6d05c6df62ba8c3ef65ff0
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-07-27 18:10:37 -0500

    image and fadeImage

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -11,7 +11,9 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(),
+          _cardTipo2()
         ],
       ),
     );
@@ -40,6 +42,31 @@ class CardPage extends StatelessWidget {
               )
             ],
           )
+        ],
+      ),
+    );
+
+  }
+
+  Widget _cardTipo2(){
+
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage('https://onbeing.org/wp-content/uploads/2017/08/2017-John-ODonohue-lead-1.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(microseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          //Image(
+          //  image: NetworkImage('https://onbeing.org/wp-content/uploads/2017/08/2017-John-ODonohue-lead-1.jpg'),
+          //),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Texto de la imagen')
+          ),
         ],
       ),
     );
