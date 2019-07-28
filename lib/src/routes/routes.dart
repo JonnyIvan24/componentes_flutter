c9a2e2bcc46f76aed3f41e257379d0ba317bf585
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-07-27 19:56:24 -0500

    Container y AnimatedContainer

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:componentes/src/pages/Home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 
 
 Map<String, WidgetBuilder> getAplicationRoutes(){
@@ -13,6 +14,7 @@ Map<String, WidgetBuilder> getAplicationRoutes(){
     'alert' : (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card'  : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
   };
 
 }
